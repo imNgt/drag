0e40f4de09bd5f3a4329701ed0d1c8c5d257859c
imNgt <imnonggt@gemail.com>
2016-08-08 14:04:35 +0800

    fetch

--- Msg.java
+++ Msg.java
@@ -3,9 +3,14 @@ public class Msg
 	public static void main(String[] args){
 		System.out.println("imNgt");
 	}
-	class singer{
+	class Singer{
 		String name;
 		int age;
 		String song;
+		Singer(){
+			this.name="imNgt";
+			this.ag=20;
+			this.song="以后的以后";
+		}
 	}
 }
